0bb8d177f0194d5d8561992021f35d61402fe476
= <skpjony@gmail.com>
2026-06-17 00:17:49 +0530

    feat: restore dynamic form logic and razorpay integration to Gokwik UI

---
+++ build_v3.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+
+const gokwikHtml = fs.readFileSync('gokwik-checkout-v2.html', 'utf8');
+
+const styleMatch = gokwikHtml.match(/<style>([\s\S]*?)<\/style>/);
+const css = styleMatch ? styleMatch[1] : '';
+
+const jsMatch = gokwikHtml.match(/<script>([\s\S]*?)<\/script>/);
+const js = jsMatch ? jsMatch[1] : '';
+const confettiStart = js.indexOf('/* ── CONFETTI');
+const confettiEnd = js.indexOf('/* ── RECALC');
+let confettiJs = '';
+if (confettiStart !== -1 && confettiEnd !== -1) {
+  confettiJs = js.substring(confettiStart, confettiEnd);
+}
+
+let oldWidget = fs.readFileSync('old_widget_utf8.js', 'utf8');
+
+const openStartStr = 'open: async function(options) {';
+const openStartIdx = oldWidget.indexOf(openStartStr);
+const autoInjectIdx = oldWidget.indexOf('autoInject: function() {');
+
+let newOpenFunction = fs.readFileSync('template_v3.js', 'utf8');
+
+newOpenFunction = newOpenFunction.replace('/*__CSS__*/', css);
+newOpenFunction = newOpenFunction.replace('/*__JS__*/', confettiJs);
+
+const finalJs = oldWidget.substring(0, openStartIdx) + newOpenFunction + '\n    ' + oldWidget.substring(autoInjectIdx);
+
+fs.writeFileSync('public/widget.js', finalJs);
+console.log('public/widget.js successfully generated using proper logic!');
